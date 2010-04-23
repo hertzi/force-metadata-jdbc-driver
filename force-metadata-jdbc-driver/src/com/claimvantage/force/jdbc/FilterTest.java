217490f1b89acc90613963497ea69b87efbba755
keith.clarke.claimvantage <keith.clarke.claimvantage@3129d375-e693-2aef-bccf-14304e57787d>
2010-04-23 08:35:17 +0000

    More filtering options.

--- force-metadata-jdbc-driver/src/com/claimvantage/force/jdbc/FilterTest.java
+++ force-metadata-jdbc-driver/src/com/claimvantage/force/jdbc/FilterTest.java
@@ -14,36 +14,60 @@ public class FilterTest extends TestCase {
         
         assertEquals(false, f.accept(createStandard("Abc")));
         assertEquals(true, f.accept(createCustom("Abc")));
-        assertEquals(true, f.accept(createCustom("USER")));
+        assertEquals(false, f.accept(createCustom("USER")));
         assertEquals(true, f.accept(createCustom("event")));
     }
     
     public void testStandard() {
         
         Properties p = new Properties();
-        p.put("exclude", "User,Event");
+        p.put("excludes", "User,Event");
         p.put("standard", "true");
         p.put("custom", "false");
         Filter f = new Filter(p);
         
         assertEquals(true, f.accept(createStandard("Abc")));
         assertEquals(false, f.accept(createCustom("Abc")));
-        assertEquals(false, f.accept(createStandard("USER")));
-        assertEquals(false, f.accept(createStandard("event")));
+        assertEquals(false, f.accept(createStandard("User")));
+        assertEquals(false, f.accept(createStandard("Event")));
     }
     
     public void testCustom() {
         
         Properties p = new Properties();
-        p.put("exclude", " User, Event ");
+        p.put("excludes", " User, Event ");
         p.put("standard", " fALsE");
         p.put("custom", " tRuE ");
         Filter f = new Filter(p);
 
         assertEquals(false, f.accept(createStandard("Abc")));
         assertEquals(true, f.accept(createCustom("Abc")));
-        assertEquals(false, f.accept(createStandard("USER")));
-        assertEquals(false, f.accept(createStandard("event")));
+        assertEquals(false, f.accept(createStandard("uSeR")));
+        assertEquals(false, f.accept(createStandard("EvEnt")));
+    }
+    
+    public void testNoExcludes() {
+        
+        Properties p = new Properties();
+        p.put("standard", "true");
+        p.put("excludes", "");
+        Filter f = new Filter(p);
+
+        assertEquals(true, f.accept(createStandard("User")));
+    }
+    
+    public void testIncludes() {
+        
+        Properties p = new Properties();
+        p.put("standard", "false");
+        // Excludes takes priority so default excludes must be removed
+        p.put("excludes", "");
+        p.put("includes", "User,Event");
+        Filter f = new Filter(p);
+
+        assertEquals(true, f.accept(createStandard("User")));
+        assertEquals(true, f.accept(createStandard("Event")));
+        assertEquals(false, f.accept(createStandard("Other")));
     }
 
     private DescribeGlobalSObjectResult createStandard(String name) {
