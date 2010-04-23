97c5d70e3637942d688b5d5adeb3c57544068e89
keith.clarke.claimvantage <keith.clarke.claimvantage@3129d375-e693-2aef-bccf-14304e57787d>
2010-04-23 13:19:14 +0000

    Change version methods to return 1.2.

--- force-metadata-jdbc-driver/src/com/claimvantage/force/jdbc/ForceMetaData.java
+++ force-metadata-jdbc-driver/src/com/claimvantage/force/jdbc/ForceMetaData.java
@@ -18,7 +18,7 @@ public class ForceMetaData implements DatabaseMetaData {
     }
 
     public String getDriverVersion() throws SQLException {
-        return "1.1";
+        return "1.2";
     }
     
     public String getDatabaseProductName() throws SQLException {
