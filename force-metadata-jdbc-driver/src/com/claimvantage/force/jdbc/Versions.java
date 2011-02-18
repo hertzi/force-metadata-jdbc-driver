9d4d9425bd448d5c69900091643a665941ac3673
keith.clarke.claimvantage <keith.clarke.claimvantage@gmail.com>
2011-02-18 23:31:15 +0000

    Consistent version numbers

--- force-metadata-jdbc-driver/src/com/claimvantage/force/jdbc/Versions.java
+++ force-metadata-jdbc-driver/src/com/claimvantage/force/jdbc/Versions.java
@@ -6,5 +6,11 @@ public class Versions {
     public static final int DRIVER_MINOR = 0;
     public static final String DRIVER_VERSION = DRIVER_MAJOR + "." + DRIVER_MINOR;
     
-    public static final String DATABASE_PRODUCT_VERSION = "21";
+    public static final int DATABASE_PRODUCT_MAJOR = 21;
+    public static final int DATABASE_PRODUCT_MINOR = 0;
+    public static final String DATABASE_PRODUCT_VERSION = String.valueOf(DATABASE_PRODUCT_MAJOR);
+    
+    // Although Java 1.6 methods have been added, currently compiled to Java 1.5 which is JDBC 3.0
+    public static final int JDBC_MAJOR = 3;
+    public static final int JDBC_MINOR = 0;
 }
